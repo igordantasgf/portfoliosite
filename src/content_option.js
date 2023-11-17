e2397a403111a2758b572d4bcbecc6eb5b56be3b
igordantasgf <igordantasgf@gmail.com>
2023-11-17 03:46:28 -0300

    update

---
+++ src/content_option.js
@@ -0,0 +1,163 @@
+const logotext = "IGOR";
+const meta = {
+    title: "Igor Dantas",
+    description: "I’m Igor Dantas data science student _ currently looking for a job from Salvador, Brazil",
+};
+
+const introdata = {
+    title: "Hi! I'm Igor Dantas",
+    animated: {
+        first: "Studying computer science!",
+        second: "I'm a Python enthusiast",
+        third: "Welcome to my web-page",
+    },
+    description: "Thanks for visiting. Take a look at some of my projects, and contact me if you like it!",
+    your_img_url: "https://uploaddeimagens.com.br/images/004/603/171/original/IMG-20221105-WA0024.jpg?1694417675",
+};
+
+const dataabout = {
+    title: "About myself",
+    aboutme: "I'm passionate for topics related with Network, Data Science, Software Engineering and, specially, Python",
+};
+const worktimeline = [{
+        where: "Research, Federal University of Bahia",
+        job: "With the project called 'Environmental Monitoring of the Salvador Metropolitan Region', we've made some data analysis on measured data from UFBA's lab (LabMiM), compared with other physical models (WRF) and deployed new types of visualization for a Fly Eye set of sensors",
+        skills: "Python, DataViz, Math, Excel, Big Data",
+        date: "09/2021 09/2023",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Flask",
+        value: 75,
+    },
+    {
+        name: "Docker",
+        value: 75,
+    },
+    {
+        name: "SQL",
+        value: 80,
+    },
+    {
+        name: "Data Analysis",
+        value: 85,
+    },
+    {
+        name: "Excel",
+        value: 75,
+    },
+    {
+        name: "Machine Learning",
+        value: 70,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "name@domain.com",
+    YOUR_FONE: "(555)123-4567",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/igordantasgf",
+    linkedin: "https://www.linkedin.com/in/igor-dantas-a288b518a/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
